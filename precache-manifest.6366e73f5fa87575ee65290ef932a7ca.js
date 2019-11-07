2501cff813c779f85915a4567f601b84dfc190d2
awaheed <awaheed@utexas.edu>
2019-11-07 11:39:48 -0600

    Updates

---
+++ precache-manifest.6366e73f5fa87575ee65290ef932a7ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae41612efa01d4f3a4cf5dee15057015",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "731f4f99f9af03d4672b",
+    "url": "/clicky-game/static/css/main.a9018cdd.chunk.css"
+  },
+  {
+    "revision": "8ddf23b8ae8e7f5c8847",
+    "url": "/clicky-game/static/js/2.19c06209.chunk.js"
+  },
+  {
+    "revision": "731f4f99f9af03d4672b",
+    "url": "/clicky-game/static/js/main.b1a32a0b.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
